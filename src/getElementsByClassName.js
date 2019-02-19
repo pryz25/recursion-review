fc9fd0a973a32c838d7e9c452caabbe02d253b7a
pryz25 <pryzanych@gmail.com>
2019-02-19 11:56:52 -0800

    "Partial solution to Get all Elements by Class Name"

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,27 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
+var getElementsByClassName = function(className, node
 ) {
-  // your code here
+  var node = node || document.body;
+  
+  var targetNodes = [];
+  
+  var getNodes = function(className, node){
+    var childNodes = node.childNodes;
+    for (var i = 0; i < childNodes.length; i++) {
+      if (childNodes[i].className === className) {
+          targetNodes.push(childNodes[i])
+      }
+      getNodes(className, childNodes[i]);
+  }
+  
+  }
+  // You should use document.body, element.childNodes, and element.classList
+  // For each node in the DOM tree:
+    // See if node has target class name...
+      // if so, get node name
+    // if not, continue
+    // for each child node...
+      // recursively run getElementsByClassName to get child nodes with target class name
 };
