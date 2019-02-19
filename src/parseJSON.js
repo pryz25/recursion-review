a313f9be48c1a657e51733af97a44a9b32145680
pryz25 <pryzanych@gmail.com>
2019-02-19 15:57:33 -0800

    "Progress against parseJSON"

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,36 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  //find overarching data structure (e.g., array, object) to parse
+  // call relevant function to begin parsing that type
+
+  var grabber = function (str) {
+    let firstChar = str[0];
+    if (firstChar === '{') {return str.slice(0, str.indexOf('}')+1)}
+    if (firstChar === '[') {return str.slice(0, str.indexOf(']')+1)}
+    if (firstChar === '"') {return str.slice(0, str.indexOf('"')+1)}
+    return str.indexOf(',') > str.indexOf(':')? str.slice(0, str.indexOf(',')) : str.slice(0, str.indexOf(':'))
+    }
+    // use first character to find end character....
+      // arr ]
+      // obj }
+      // str ""
+      // else comma, colon, or end of length
+  return grabber(json)
+  /* Object Function
+    set new variable object {}
+    determine if object has key value pairs (Assess if there are colons present in the passed value)
+      if so, call string function on first key and set to variable
+      assess if value at property is 
+
+  */
+
+  /*
+  Attempt at Grammar
+
+
+
+
+
+  */
 };
